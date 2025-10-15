a22e2d67bde7ef27b90b20444b22608a6547a1f8
PaulAjii <paulajii134@gmail.com>
2025-10-15 14:29:59 +0100

    feat: created hospital type

---
+++ src/types/hospital.d.ts
@@ -0,0 +1,15 @@
+export interface IHospital {
+  id?: string;
+  state: string;
+  lga: string;
+  hospitalName: string;
+  tier: string;
+  address: string;
+  phone_number: string;
+  email_address: string;
+  password: string;
+  healthCareWorkers: string;
+  patients: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
