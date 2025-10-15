3c39e7b5c69cbac68a8e276248819c5c4a26960d
PaulAjii <paulajii134@gmail.com>
2025-10-15 13:55:31 +0100

    feat: added error handling middleware

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from "express";
+
+interface CustomError extends Error {
+  statusCode?: number;
+  code?: number;
+  errors?: Record<string, { message: string }>;
+}
+
+const errorMiddleware = (
+  err: CustomError,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  try {
+    const error = { ...err };
+    error.message = err.message;
+
+    // Error Types
+    // 1. Mongoose bad ObjectId
+    if (err.name === "CastError") {
+      error.message = "Resource not found";
+      error.statusCode = 404;
+    }
+
+    // 2. Mongoose duplicate key
+    if (err.code === 11000) {
+      error.message = `Duplicate field value entered`;
+      error.statusCode = 400;
+    }
+
+    // 3. Mongoose validation error
+    if (err.name === "ValidationError" && err.errors) {
+      const messages = Object.values(err.errors).map((val) => val.message);
+      error.message = messages.join(", ");
+      error.statusCode = 400;
+    }
+
+    res.status(error.statusCode || 500).json({
+      status: "error",
+      message: error.message || "Server Error",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default errorMiddleware;
